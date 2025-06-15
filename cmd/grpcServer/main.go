706e56506f72dedc6aeecfc18d7b35f1a3c352e1
Caio Bomfim <caaiobomfim@gmail.com>
2025-06-14 21:36:53 -0300

    :rocket: project: first commit

---
+++ cmd/grpcServer/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"net"
+
+	"github.com/caaiobomfim/project-grpc/internal/database"
+	"github.com/caaiobomfim/project-grpc/internal/pb"
+	"github.com/caaiobomfim/project-grpc/internal/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	db, err := sql.Open("sqlite", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDb := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDb)
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	reflection.Register(grpcServer)
+
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+}
